b4ec8b5b7d3013329f174618089ff66615d0f5f0
Gurmeet Singh <gurmeet.aurora@gmail.com>
2025-05-17 07:46:12 +0530

    initial commit

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center p-24">
+      <h1 className="text-2xl font-bold">Customers Page!</h1>
+    </div>
+  );
+}
