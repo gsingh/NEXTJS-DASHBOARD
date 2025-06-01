64b73ee7750c6b9ebbbf39648809e38cab29ab71
Gurmeet Singh <gurmeet.aurora@gmail.com>
2025-06-01 22:46:46 +0530

    3rd commit 1-jun

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -8,10 +8,11 @@ import CardWrapper from '@/app/ui/dashboard/cards';
 import { CardSkeleton } from '@/app/ui/skeletons';
 import { fetchCardData } from '@/app/lib/data';
 
+
     export default async function Page() {
     // const revenue = await fetchRevenue();
 
-    const { numberOfCustomers, numberOfInvoices, totalPaidInvoices, totalPendingInvoices } = await fetchCardData();
+   
   return (
     <main>
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
@@ -21,14 +22,8 @@ import { fetchCardData } from '@/app/lib/data';
       <Suspense fallback={<CardSkeleton />}>
           <CardWrapper />
         </Suspense>
-        {/* <Card title="Collected" value={totalPaidInvoices} type="collected" />
-        <Card title="Pending" value={totalPendingInvoices} type="pending" />
-        <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
-        <Card
-          title="Total Customers"
-          value={numberOfCustomers}
-          type="customers"
-        /> */}
+     
+      
 
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
